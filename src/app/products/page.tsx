5de68b8ae5f6042d23f1e4fec49367a6c666f40b
santiagoccaa <santiagoccaa@gmail.com>
2025-09-30 21:18:18 -0500

    resolve conflic

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,28 +1,41 @@
-import Image from "next/image"
+// import Image from "next/image"
 
 
-async function GetProducts() {
-    const res = await fetch('http://localhost:1337/api/menu-products?populate=*')
-    const data = await res.json()
-    return data
+// async function GetProducts() {
+//     const res = await fetch('http://localhost:1337/api/menu-products?populate=*')
+//     const data = await res.json()
+//     return data
+// }
+
+// const ProductsPage = async () => {
+//     const { data: p } = await GetProducts()
+//     console.log("Productos");
+
+//     console.log(p);
+
+//     return (
+//         <div className='h-screen bg-black flex justify-center items-center'>
+//             {p.map((p) => (
+//                 <div key={p.id}>
+//                     <h1>{p.title}</h1>
+//                     <Image src={`http://localhost:1337${p.image.url}`} width={800} height={800} alt="image product" className="w-48 h-48" />
+//                 </div>
+//             ))}
+//         </div>
+//     )
+// }
+
+// export default ProductsPage
+
+import React from 'react'
+
+const PageProduct = () => {
+  return (
+    <div>
+        Hola mundo
+    </div>
+  )
 }
 
-const ProductsPage = async () => {
-    const { data: p } = await GetProducts()
-    console.log("Productos");
+export default PageProduct
 
-    console.log(p);
-
-    return (
-        <div className='h-screen bg-black flex justify-center items-center'>
-            {p.map((p) => (
-                <div key={p.id}>
-                    <h1>{p.title}</h1>
-                    <Image src={`http://localhost:1337${p.image.url}`} width={800} height={800} alt="image product" className="w-48 h-48" />
-                </div>
-            ))}
-        </div>
-    )
-}
-
-export default ProductsPage
